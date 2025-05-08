f077f8998cb3e904870a4e3d5ac34325ad032768
alasiri <austinalasiri44@gmail.com>
2025-05-08 16:04:17 +0300

    added messages

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+import math
 
 def return_evens(num_list):
-    pass
+    num_list = [n for n in num_list if math.remainder(n, 2) == 0]
+    return num_list
 
 def make_exclamation(sentence_list):
-    pass
+    sentence_list = [n + '!' for n in sentence_list]
+    return sentence_list
